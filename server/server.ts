f0cfccfdc6677b0477157c442cb76f70ddb901eb
Jerry Thimothy <jerrythimothy@gmail.com>
2020-07-24 15:37:43 +0400

    Setup validation layer #9

--- server/server.ts
+++ server/server.ts
@@ -1,5 +1,6 @@
 import express = require('express');
 import axios from "axios"
+const { query, validationResult, checkSchema } = require('express-validator');
 
 // Create a new express app instance
 const app: express.Application = express();
@@ -8,20 +9,43 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.get('/api/search', async (req, res) => {
-    const { text, type } = req.query;
+app.get(
+    '/api/search',
+    [
+        checkSchema({
+            text: {
+                isLength: {
+                    errorMessage: 'Search text should be 3 or more characters',
+                    options: { min: 3 }
+                }
+            },
+            type: {
+                matches: {
+                    errorMessage: 'Search type should be either users, repositories or issues',
+                    options: [/\b(?:users|repositories|issues)\b/],
+                }
+            }
+        })
+    ],
+    async (req: any, res: any) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
 
-    try {
-        const usersRes = await axios.get(
-            `https://api.github.com/search/${type}?q=${text}`,
-            { headers: { Authorization: `token 8db0c1b68f1198949a7dfafd7b2dbd110a721e29` } }
-        )
-        return res.send(usersRes.data);
-    }
-    catch (err) {
-        return res.send(err);
-    }
-});
+        const { text, type } = req.query;
+
+        try {
+            const usersRes = await axios.get(
+                `https://api.github.com/search/${type}?q=${text}`,
+                { headers: { Authorization: `token 8db0c1b68f1198949a7dfafd7b2dbd110a721e29` } }
+            )
+            return res.send(usersRes.data);
+        }
+        catch (err) {
+            return res.send(err);
+        }
+    });
 
 
 app.listen(4000, function () {
