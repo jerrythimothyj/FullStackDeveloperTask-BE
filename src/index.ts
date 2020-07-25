a0abc680a5f800fb037c37d03875977a0ad41d4e
Jerry Thimothy J <jerrythimothy@gmail.com>
2020-07-25 21:25:17 +0400

    Setup swagger documentation #3
    * Setup swagger documentation #3
    
    * Checking swagger
    
    * More code cleanup

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,40 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 import express from 'express';
+import bodyParser from 'body-parser'
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 import search from './routes/search.route';
 import cache from './routes/cache.route';
 
 const port = Number(process.env.PORT);
 
 const app: express.Application = express();
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+const swaggerOptions = {
+    swaggerDefinition: {
+        info: {
+            version: "1.0.0",
+            title: "Github API",
+            description: "Github API Information",
+            contact: {
+                name: "Jerry"
+            },
+            servers: [`http://localhost:${process.env.PORT}`]
+        }
+    },
+    apis: ["dist/routes/**/*.js"]
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
 app.use('/', search);
 app.use('/', cache);
+
+
 
 app.listen(port, function () {
     console.log(`App is listening on port ${process.env.PORT}!`);
