4f67d14ab49aca1f005e0b02c91915f7adafdbd0
Jerry Thimothy <jerrythimothy@gmail.com>
2020-07-29 00:45:32 +0400

    Setup open cors #28

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: process.env.GITHUB_BASE_URL,
-    headers: { Authorization: `token ${process.env.GITHUB_TOKEN}` }
+    headers: { Authorization: `token ${process.env.GITHUB_TOKEN}`, accept: 'application/vnd.github.v3+json' }
 });
 
 export const apiGet = (url: string, params: { text: string, page: number, per_page: number }) => instance.get(
