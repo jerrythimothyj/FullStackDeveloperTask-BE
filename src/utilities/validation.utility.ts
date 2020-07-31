d98eb9825d9291340826bb7d46e1c16b6fd8d57d
Jerry Thimothy J <jerrythimothy@gmail.com>
2020-07-31 17:42:01 +0400

    Fix lint errors #25

--- src/utilities/validation.utility.ts
+++ src/utilities/validation.utility.ts
@@ -1,6 +1,6 @@
-import { validationResult, checkSchema } from 'express-validator';
+import { validationResult } from 'express-validator';
 
-export const throwValidationError = (req: any, res: any) => {
+export const throwValidationError = (req: any, res: any): any => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
